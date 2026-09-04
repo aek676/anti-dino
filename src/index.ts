ef4a93a851d102cd9fc7c821c3ff903e18f7fd4c
Anass El Jabiry <aek676@inlumine.ual.es>
2026-09-04 23:57:02 +0200

    feat: close the database on SIGINT and SIGTERM

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import { Elysia } from "elysia";
+import { closeDatabase, db } from "@/utils/db";
 import { env } from "@/utils/env";
 import { log } from "@/utils/logger";
 
-new Elysia()
+const app = new Elysia()
 	.use(log.into({ autoLogging: true }))
+	.onStop(() => {
+		closeDatabase(db);
+	})
 	.get("/", () => "Hello Elysia")
 	.listen(env.PORT);
+
+const shutdown = (signal: NodeJS.Signals): void => {
+	log.info({ signal }, "shutting down");
+	void app.stop();
+};
+
+process.once("SIGINT", shutdown);
+process.once("SIGTERM", shutdown);
