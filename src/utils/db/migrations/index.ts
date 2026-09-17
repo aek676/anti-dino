88d2e13b774ce15a81461c357db63b794cbb3999
aek676 <aek676@inlumine.ual.es>
2026-09-17 14:55:46 +0200

    Track sent alerts in a new alerts table

--- src/utils/db/migrations/index.ts
+++ src/utils/db/migrations/index.ts
@@ -1,5 +1,6 @@
 import { subscribers } from "@/utils/db/migrations/001-subscribers";
 import { slots } from "@/utils/db/migrations/002-slots";
+import { alerts } from "@/utils/db/migrations/003-alerts";
 import type { Migration } from "@/utils/db/types";
 
-export const migrations: readonly Migration[] = [subscribers, slots];
+export const migrations: readonly Migration[] = [subscribers, slots, alerts];
